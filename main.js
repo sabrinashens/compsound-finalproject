7e2980d2bdde4c2fadf3811164800db811c8692f
Xinyu Shen <sabrinashen@229.155.182.163.naturalwireless.com>
2022-12-04 00:39:53 -0500

    loops

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ let notetoPitch = {
 };
 
 let scaletoNum = {
-	"3rd": 2,
+    "3rd": 2,
     "5th": 3,
     "7th": 4,
     "9th": 5,
@@ -32,46 +32,57 @@ let majorScale = [0, 4, 7, 11, 14, 17, 21];
 let minorScale = [0, 3, 7, 10, 14, 17, 21];
 
 function parseCode(code) {
-	let chords = code.split(" ");
+    let chords = code.split(" "); //e.g., 2@C:9th[4]
+
     chords = chords.map(chord => {
-    	let scale = chord.split(":");
-        let root = scale[0].split("@")
-        let length = root[0];
-        scale = scale[1];
-        root = root[1];
-        let nth = scaletoNum[scale];
-        let chordMelody = new Array(nth).fill(0);
-        
+        let noteCode = chord.split(":"); //[2@C, 9th[4]]
+
+        let scaleNloops = noteCode[1].split("["); //[9th, 4]]
+        let loops = scaleNloops[1].split("]"); //[4, ]]
+        loops = loops[0]; //4
+        let scale = scaleNloops[0]; //9th
+        let nth = scaletoNum[scale]; //mapped into 5
+
+        let root = noteCode[0].split("@"); //[2, C]
+        let length = root[0]; //2
+        root = root[1]; //C
+
+        let chordMelody = new Array(nth).fill(0); //[0, 0, 0, 0, 0]
         if (root.at(-1) == "m") { 
             //if a minor chord
-        	root = root.slice(0,-1);
+            root = root.slice(0,-1);
             let pitch = notetoPitch[root];
-            
+
             for (var i = 0; i < nth; i++) {
-               chordMelody[i] = minorScale[i] + pitch;
+               chordMelody[i] = minorScale[i] + pitch; 
             }
         }  
         else { 
             //if a major chord
-        	let pitch = notetoPitch[root];
+            let pitch = notetoPitch[root]; //mapped into 60
+
             for (var i = 0; i < nth; i++) {
-            	chordMelody[i] = majorScale[i] + pitch;
-            }
+                chordMelody[i] = majorScale[i] + pitch;
+            }// [60, 64, 67, 71, 74]
         }
-    	return {
-            "length": length,
-            "notes": chordMelody, 
-      	};
+
+        return {
+            "length": length*1,
+            "notes": chordMelody,
+            "loops": loops*1
+        };
     });
+
     return chords;
 }
 
 function genSequence(length, note, tempo) {
     sequence.push({
-    	note: note,
+        note: note,
         startTime: startTime,
         endTime: startTime + length/tempo
     });
+
     startTime += length/tempo;  
     return sequence
 }
@@ -80,23 +91,27 @@ const playButton = document.getElementById('play');
 playButton.addEventListener("click", function () {
     code = document.getElementById('code').value;
     tempo = document.getElementById('tempo').value / 10;
+
     var chords = parseCode(code);
+    
     sequence = [];
     startTime = 0;
-	for (var i = 0; i < chords.length; i++) {
-        let length = (chords[i].length);
-        let chordNotes= (chords[i].notes);
+    for (var i = 0; i < chords.length; i++) {
+        let length = chords[i].length;
+        let chordNotes= chords[i].notes;
+        chordNotes = new Array(chords[i].loops).fill(chordNotes).flat();
         for (var j = 0; j < chordNotes.length; j++) {
             genSequence(length, chordNotes[j], tempo)
         }
-	}
-    
+    }
+
     audioCtx = new (window.AudioContext || window.webkitAudioContext);
     osc = audioCtx.createOscillator();
     gainNode = audioCtx.createGain();
     osc.connect(gainNode).connect(audioCtx.destination);
     osc.start();
     gainNode.gain.value = 0;
+
     playNotes(sequence);
 })
 
@@ -111,9 +126,18 @@ function playNotes(noteList) {
 }
 
 function playNote(note) {
-	offset = 1;
+    offset = 1;
     gainNode.gain.setTargetAtTime(0.7, note.startTime + offset, 0.01)
     osc.frequency.setTargetAtTime(midiToFreq(note.note),
     note.startTime + offset, 0.001)
     gainNode.gain.setTargetAtTime(0, note.endTime + offset - 0.05, 0.01)
 }
+
+function copyExample() {
+    var copyText = document.getElementById("example");
+    navigator.clipboard.writeText(copyText.value);
+    document.getElementById('copy').style.color = "white";
+    document.getElementById('copy').style.backgroundColor = "black";
+    document.getElementById('copy').innerHTML = "Copied";
+}
+
