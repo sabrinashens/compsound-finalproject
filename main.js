117353f1f290e76e387500882873f1b418835ca3
Xinyu Shen <sabrinashen@229.155.182.163.naturalwireless.com>
2022-12-03 23:09:46 -0500

    audio

--- main.js
+++ main.js
@@ -1,41 +1,8 @@
-const playButton = document.getElementById('play');
-var playState = false;
+var audioCtx;
+var osc;
+var gainNode;
 var startTime;
 var endTime;
-
-document.addEventListener("DOMContentLoaded", function (event) {
-
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    
-    playButton.addEventListener("click", function () {
-    	if (playState == false) {
-            code = document.getElementById('code').value;
-            tempo = document.getElementById('tempo').value / 50;
-            
-            var chords = parseCode(code);
-            
-            sequence = [];
-            startTime = 0;
-			for (var i = 0; i < chords.length; i++) {
-                let length = (chords[i].length);
-                let chordNotes= (chords[i].notes);
-                for (var j = 0; j < chordNotes.length; j++) {
-                    genSequence(length, chordNotes[j], tempo)
-                }
-			}
-            console.log(sequence);
-
-            document.getElementById('play').style.backgroundColor = "black";
-            document.getElementById('play').style.color = "white"; 
-            playState = true;
-        }
-        else if (playState == true) {            
-            document.getElementById('play').style.backgroundColor = "white";
-            document.getElementById('play').style.color = "black"; 
-            playState = false;
-        } 
-    })
-})
 
 let notetoPitch = {
     "C": 60,
@@ -66,7 +33,6 @@ let minorScale = [0, 3, 7, 10, 14, 17, 21];
 
 function parseCode(code) {
 	let chords = code.split(" ");
-    
     chords = chords.map(chord => {
     	let scale = chord.split(":");
         let root = scale[0].split("@")
@@ -108,4 +74,46 @@ function genSequence(length, note, tempo) {
     });
     startTime += length/tempo;  
     return sequence
+}
+
+const playButton = document.getElementById('play');
+playButton.addEventListener("click", function () {
+    code = document.getElementById('code').value;
+    tempo = document.getElementById('tempo').value / 10;
+    var chords = parseCode(code);
+    sequence = [];
+    startTime = 0;
+	for (var i = 0; i < chords.length; i++) {
+        let length = (chords[i].length);
+        let chordNotes= (chords[i].notes);
+        for (var j = 0; j < chordNotes.length; j++) {
+            genSequence(length, chordNotes[j], tempo)
+        }
+	}
+    
+    audioCtx = new (window.AudioContext || window.webkitAudioContext);
+    osc = audioCtx.createOscillator();
+    gainNode = audioCtx.createGain();
+    osc.connect(gainNode).connect(audioCtx.destination);
+    osc.start();
+    gainNode.gain.value = 0;
+    playNotes(sequence);
+})
+
+function midiToFreq(m) {
+    return Math.pow(2, (m - 69) / 12) * 440;
+}
+
+function playNotes(noteList) {
+    noteList.forEach(note => {
+        playNote(note);
+    }); 
+}
+
+function playNote(note) {
+	offset = 1;
+    gainNode.gain.setTargetAtTime(0.7, note.startTime + offset, 0.01)
+    osc.frequency.setTargetAtTime(midiToFreq(note.note),
+    note.startTime + offset, 0.001)
+    gainNode.gain.setTargetAtTime(0, note.endTime + offset - 0.05, 0.01)
 }
